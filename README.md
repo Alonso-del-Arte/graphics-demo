0ebcbe05a0e045d1754eda8474ff7ae15342c56d
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-08 01:35:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Graphics Demo
+
+WORK IN PROGRESS
+
+For a Medium article to demonstrate the basics of GUI programming from the Java 
+perspective.
+
+Java 21.
---
+++ test/org/example/graphics/scene/TreeNGTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package org.example.graphics.scene;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author al
+ */
+public class TreeNGTest {
+    
+    public TreeNGTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
