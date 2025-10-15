27ce8b0d43b3e6da5bd206322b297c1f5f7f600b
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-14 23:55:45 -0400

    Getter, constructor stubs

--- src/org/example/graphics/scene/Tree.java
+++ src/org/example/graphics/scene/Tree.java
@@ -1,13 +1,44 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright (C) 2025 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.example.graphics.scene;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+
 /**
  *
- * @author al
+ * @author Alonso del Arte
  */
 public class Tree {
+    
+    // TODO: Write tests for this
+    public Color getLeafColor() {
+        return Color.WHITE;
+    }
+    
+    // TODO: Write tests for this
+    public Dimension getDimension() {
+        return new Dimension(1, 1);
+    }
+    
+    public Tree(Color leafColor, Dimension size) {
+        //
+    }
     
 }
