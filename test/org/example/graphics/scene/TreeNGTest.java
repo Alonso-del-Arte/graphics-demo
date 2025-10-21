187313afc947bad301a8d664f35543df3a1d00dc
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-20 23:50:10 -0400

    Failing getLeafColor( ) test

--- test/org/example/graphics/scene/TreeNGTest.java
+++ test/org/example/graphics/scene/TreeNGTest.java
@@ -1,25 +1,50 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ * Copyright (C) 2025 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.example.graphics.scene;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import static org.example.randomness.ExtendedRandom.nextColor;
 
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 /**
- *
- * @author al
+ * Tests of the Tree class.
+ * @author Alonso del Arte
  */
 public class TreeNGTest {
     
-    public TreeNGTest() {
-    }
-
+    private static final int DEFAULT_WIDTH = 50;
+    
+    private static final int DEFAULT_HEIGHT = 100;
+    
+    private static final Dimension DEFAULT_DIMENSION 
+            = new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testGetLeafColor() {
+        Color expected = nextColor();
+        Tree instance = new Tree(expected, DEFAULT_DIMENSION);
+        Color actual = instance.getLeafColor();
+        assertEquals(actual, expected);
     }
     
 }
