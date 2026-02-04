2faaf79ad4c4fe6487145c80cb16a2d08fbb58d0
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-03 22:55:31 -0500

    New test package

---
+++ test/org/example/graphics/demo/TreePanelNGTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2026 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.example.graphics.demo;
+
+import java.awt.Graphics;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ * Tests of the TreePanel class.
+ * @author Alonso del Arte
+ */
+public class TreePanelNGTest {
+    
+    /**
+     * Test of changeNumberOfTrees method, of class TreePanel.
+     */
+    @Test
+    public void testChangeNumberOfTrees() {
+        System.out.println("changeNumberOfTrees");
+        byte treeCount = 0;
+        TreePanel instance = null;
+//        instance.changeNumberOfTrees(treeCount);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of paintComponent method, of class TreePanel.
+     */
+    @Test
+    public void testPaintComponent() {
+        System.out.println("paintComponent");
+        Graphics g = null;
+        TreePanel instance = null;
+//        instance.paintComponent(g);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
