9b1b44f523cd416bbd46e61ba597576bd2ade97b
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-03 22:49:01 -0500

    Various preparations

--- src/org/example/graphics/demo/GraphicsDemo.java
+++ src/org/example/graphics/demo/GraphicsDemo.java
@@ -1,15 +1,43 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Copyright (C) 2026 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.example.graphics.demo;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
 /**
  *
- * @author al
+ * @author Alonso del Arte
  */
-public class GraphicsDemo {
+public final class GraphicsDemo extends JFrame {
 
+    // TODO: Write a test for this
+    public static final byte DEFAULT_NUMBER_OF_TREES = -1;
+    
+    public GraphicsDemo() {
+        this(DEFAULT_NUMBER_OF_TREES);
+    }
+    
+    public GraphicsDemo(byte initialNumberOfTrees) {
+        //
+    }
+    
     /**
      * @param args the command line arguments
      */
