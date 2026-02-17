b9eab82faffd061701ea0554cc31ec2653032681
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-17 00:38:41 -0500

    Passing getLeafColor( ) test for instance from 1-param constructor

--- src/org/example/graphics/scene/Tree.java
+++ src/org/example/graphics/scene/Tree.java
@@ -81,7 +81,7 @@ public class Tree {
 
     // TODO: Write tests for this
     public Tree(Dimension size) {
-        this.upperColor = Color.WHITE;
+        this.upperColor = DEFAULT_LEAF_COLOR;
         this.lowerColor = Color.LIGHT_GRAY;
         this.lowLeafLevel = -1;
     }
