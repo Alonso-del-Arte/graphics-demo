7d312b9f33444a6c0ea0081c37d54683855113f2
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-23 11:47:12 -0500

    Javadoc for nextInt( ) functions

--- src/org/example/randomness/ExtendedRandom.java
+++ src/org/example/randomness/ExtendedRandom.java
@@ -31,14 +31,58 @@ public class ExtendedRandom {
     
     private static final Random RANDOM = new Random();
     
+    /**
+     * Provides a pseudorandom integer. If an integer that is not negative is 
+     * desired, it might be preferable to use {@link #nextInt(int) 
+     * nextInt(bound)}. For an integer between a specified origin and a 
+     * specified bound, use {@link #nextInt(int, int) nextInt(origin, bound)}.
+     * @return Some 32-bit integer. Examples: &minus;1432737928, 224929558, 
+     * &minus;1362698030, 1644402551. The values &minus;2<sup>31</sup>, 0 and 
+     * 2<sup>31</sup> &minus; 1 are possible but unlikely.
+     */
     public static int nextInt() {
         return RANDOM.nextInt();
     }
     
+    /**
+     * Provides a pseudorandom nonnegative integer between 0 and a specified 
+     * positive bound. If any 32-bit integer is acceptable, use {@link 
+     * #nextInt() nextInt()} (no parameters). For an integer between a specified 
+     * origin and a specified bound, use {@link #nextInt(int, int) 
+     * nextInt(origin, bound)}.
+     * @param bound One more than the maximum integer that may be returned. Must 
+     * be positive. For example, 256. The value 1 is acceptable but kind of 
+     * pointless, since in that case this function can only return 0.
+     * @return A pseudorandom integer, at least 0 but less than {@code bound}. 
+     * Examples: 151, 250, 196, 20, 53, 126, 177. In this example, 255 might 
+     * come up, but 256 should not.
+     * @throws IllegalArgumentException If {@code bound} is 0 or a negative 
+     * integer.
+     */
     public static int nextInt(int bound) {
         return RANDOM.nextInt(bound);
     }
     
+    /**
+     * Provides a pseudorandom integer between a specified origin and bound. If 
+     * the origin is 0, it might be preferable to use {@link #nextInt(int) 
+     * nextInt(bound)}. If any 32-bit integer is acceptable, use {@link 
+     * #nextInt() nextInt()} (no parameters).
+     * @param origin The smallest integer that may be returned. May be 0, but 
+     * preferably a positive integer. For example, 128. Negative integers may 
+     * also be used. For example, &minus;128.
+     * @param bound One more than the maximum integer that may be returned. For 
+     * example, 256. Must not be equal to nor less than {@code origin}.
+     * @return An integer, at least {@code origin} but less than {@code bound}. 
+     * With the example parameters, this function may return values such as 241, 
+     * 155, 184, 204, 203, 248, 133. The values 128 and 255 may come up, but 256 
+     * should not. With the example {@code origin} of &minus;128, this function 
+     * may also return values such as 104, &minus;3, 170, &minus;6, &minus;108, 
+     * 95. In this example, &minus;128 may come up, as well as 255, but not 256.
+     * @throws IllegalArgumentException If {@code origin} is greater than {@code 
+     * bound}, or equal. For example, an {@code origin} of 256 with a {@code 
+     * bound} of 128 will trigger this exception.
+     */
     public static int nextInt(int origin, int bound) {
         return RANDOM.nextInt(origin, bound);
     }
