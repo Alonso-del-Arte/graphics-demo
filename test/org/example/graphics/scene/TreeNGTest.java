d9eef3d4a2f820a9907946ea9d8a6d4fa30ff9a9
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-05 00:13:10 -0500

    Failing test that 3-param constructor rejects dimension with width 0

--- test/org/example/graphics/scene/TreeNGTest.java
+++ test/org/example/graphics/scene/TreeNGTest.java
@@ -304,4 +304,24 @@ public class TreeNGTest {
         System.out.println("\"" + excMsg + "\"");
     }
     
+    @Test
+    public void test3ParamConstructorRejectsDimensionWithZeroWidth() {
+        int width = 0;
+        int height = nextInt(256) + 1;
+        Dimension size = new Dimension(width, height);
+        String dimStr = size.toString();
+        String msg = dimStr + " should cause exception";
+        Throwable t = assertThrows(() -> {
+            Tree badResult = new Tree(nextColor(), nextColor(), size);
+            System.out.println(msg + ", not given " + badResult.toString());
+        }, IllegalArgumentException.class, msg);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be false";
+        String containsMsg = "Exception message should contain \"" + dimStr 
+                + "\"";
+        assert excMsg.contains(dimStr) : containsMsg;
+        System.out.println("\"" + excMsg + "\"");
+    }
+    
 }
