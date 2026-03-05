2fab6781cc7d0b7452bb1d3762d9fc3e61c8d005
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-05 00:13:50 -0500

    Passing test that 3-param constructor rejects dimension with width 0

--- src/org/example/graphics/scene/Tree.java
+++ src/org/example/graphics/scene/Tree.java
@@ -103,7 +103,7 @@ public class Tree {
     
     // TODO: Write tests for this
     public Tree(Color leafColor, Color trunkColor, Dimension size) {
-        if (size.width < 0 || size.height < 0) {
+        if (size.width < 1 || size.height < 0) {
             String excMsg = size.toString() + " is not valid";
             throw new IllegalArgumentException(excMsg);
         }
